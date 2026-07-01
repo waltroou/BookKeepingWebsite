0837d8724c0804c1eb204bd7aac89de03f24f5d1
waltroou <walter.roou.21@cnu.edu>
2026-07-01 13:21:50 -0400

    Configure static export for Render

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 import { siteConfig } from "@/config/site";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
